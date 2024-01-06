1c82b6d7fc9cd51c1069b8266f23ad9a1e0915ee
Eduardo Correia <skelozard1@gmail.com>
2024-01-06 19:11:57 +0000

    Launch file for navigation

---
+++ core/launch/core.launch.py
@@ -0,0 +1,106 @@
+from launch import LaunchDescription
+from launch.actions import GroupAction
+from launch_ros.actions import Node, PushRosNamespace, SetParameter
+from launch_ros.descriptions import ParameterFile
+from nav2_common.launch import RewrittenYaml
+
+import launch_ros
+import os
+
+
+def generate_launch_description():
+    pkg_share = launch_ros.substitutions.FindPackageShare(package="core").find("core")
+    namespace = "nemo"
+    params_file = "core/config/nav2_params.yaml"
+    param_substitutions = {}
+
+    configured_params = ParameterFile(
+        RewrittenYaml(
+            source_file=params_file,
+            root_key=namespace,
+            param_rewrites=param_substitutions,
+            convert_types=True,
+        ),
+        allow_substs=True,
+    )
+
+    lifecycle_nodes = [
+        "controller_server",
+        "planner_server",
+        "bt_navigator",
+    ]
+
+    return LaunchDescription(
+        [
+            GroupAction(
+                [
+                    PushRosNamespace(namespace),
+                    LaunchDescription(
+                        [
+                            SetParameter("use_sim_time", True),
+                            # Map server
+                            Node(
+                                package="nav2_map_server",
+                                name="map_server",
+                                executable="map_server",
+                                output="screen",
+                                parameters=[configured_params, {'yaml_filename': "core/maps/map.yaml"}],
+                            ),
+                            # Controller server
+                            Node(
+                                package="nav2_controller",
+                                name="controller_server",
+                                executable="controller_server",
+                                output="screen",
+                                parameters=[
+                                    ParameterFile("core/config/controller.yaml"),
+                                    {
+                                        "FollowPath": {
+                                            "plugin": "dwb_core::DWBLocalPlanner",
+                                            "critics": [
+                                                "RotateToGoal",
+                                                "Oscillation",
+                                                "BaseObstacle",
+                                                "GoalAlign",
+                                                "PathAlign",
+                                                "PathDist",
+                                                "GoalDist",
+                                            ],
+                                        }
+                                    },
+                                ],
+                            ),
+                            # Planner server
+                            Node(
+                                package="nav2_planner",
+                                name="planner_server",
+                                executable="planner_server",
+                                output="screen",
+                                parameters=[configured_params],
+                            ),
+                            # BT navigator
+                            Node(
+                                package="nav2_bt_navigator",
+                                name="bt_navigator",
+                                executable="bt_navigator",
+                                output="screen",
+                                parameters=[configured_params],
+                            ),
+                            # Lifecycle manager
+                            Node(
+                                package="nav2_lifecycle_manager",
+                                name="lifecycle_manager",
+                                executable="lifecycle_manager",
+                                output="screen",
+                                parameters=[
+                                    {"autostart": True},
+                                    {"use_sim_time": True},
+                                    {"node_names": lifecycle_nodes},
+                                ],
+                            ),
+                        ]
+                    ),
+                ]
+            ),
+        ]
+    )
