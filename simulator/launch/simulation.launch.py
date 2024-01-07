9f3f43a75006342f127916aa23be798b76120a4d
Eduardo Correia <skelozard1@gmail.com>
2024-01-07 17:06:37 +0000

    Rviz configuration

--- simulator/launch/simulation.launch.py
+++ simulator/launch/simulation.launch.py
@@ -10,18 +10,24 @@ import os
 
 
 def generate_launch_description():
-    # ROS Gazebo Bridge
-    ros_gz_bridge_node = Node(
-        package="ros_gz_bridge",
-        executable="parameter_bridge",
-        name="ros_gz_bridge",
-        output="screen",
-        arguments=[
-            "/model/nemo/cmd_vel@geometry_msgs/msg/Twist@gz.msgs.Twist",
-            "/model/nemo/odometry@nav_msgs/msg/Odometry@ignition.msgs.Odometry",
-            "/camera@sensor_msgs/msg/Image@ignition.msgs.Image",
-            "/camera_info@sensor_msgs/msg/CameraInfo@ignition.msgs.CameraInfo",
-        ],
+    sdf_path = "data/models/nemo/model.sdf"
+
+    with open(sdf_path, 'r') as f:
+        robot_description = f.read()
+
+    # Robot State Publisher
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[{'robot_description': robot_description}],
+    )
+
+    # Joint State Publisher
+    joint_state_publisher_node = Node(
+        package="joint_state_publisher",
+        executable="joint_state_publisher",
+        name="joint_state_publisher",
+        arguments=[sdf_path],
     )
 
     # Robot Localization
@@ -33,6 +39,29 @@ def generate_launch_description():
         parameters=[
             "config/ekf.yaml",
             {"use_sim_time": True},
+        ],
+    )
+
+    # Rviz
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="screen",
+        arguments=["-d", "rviz/urdf_config.rviz"],
+    )
+
+    # ROS Gazebo Bridge
+    ros_gz_bridge_node = Node(
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
+        name="ros_gz_bridge",
+        output="screen",
+        arguments=[
+            "/model/nemo/cmd_vel@geometry_msgs/msg/Twist@gz.msgs.Twist",
+            "/model/nemo/odometry@nav_msgs/msg/Odometry@ignition.msgs.Odometry",
+            "/camera@sensor_msgs/msg/Image@ignition.msgs.Image",
+            "/camera_info@sensor_msgs/msg/CameraInfo@ignition.msgs.CameraInfo",
         ],
     )
 
@@ -54,8 +83,11 @@ def generate_launch_description():
 
     return LaunchDescription(
         [
-            ros_gz_bridge_node,
-            robot_localization_node,
-            ign_gazebo,
+            robot_state_publisher_node,
+            joint_state_publisher_node,
+            # robot_localization_node,
+            rviz_node,
+            # ros_gz_bridge_node,
+            # ign_gazebo,
         ]
     )
